1167bf31aa28523889d69f5eec93f17cf2ec655e
denis <permwed@gmail.com>
2022-03-09 09:52:49 +0500

    add favorite page

---
+++ src/containers/FavoritePage/FavoritePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+import styles from "./FavoritePage.module.css";
+
+const FavoritePage = () => {
+  const storeData = useSelector((state) => state.favoriteReducer);
+  console.log(storeData);
+
+  return <div>FavoritePage</div>;
+};
+
+export default FavoritePage;
--- src/routes/routesConfig.js
+++ src/routes/routesConfig.js
@@ -3,11 +3,13 @@ import HomePage from "../containers/HomePage/HomePage";
 import NotFoundPage from "../containers/NotFoundPage/NotFoundPage";
 import PeoplePage from "../containers/PeoplePage/PeoplePage";
 import PersonPage from "../containers/PersonPage/PersonPage";
+import FavoritePage from "../containers/FavoritePage/FavoritePage";
 
 const routesConfig = [
   { path: "/", element: <HomePage /> },
   { path: "/people", element: <PeoplePage /> },
   { path: "/people/:id", element: <PersonPage /> },
+  { path: "/favorites", element: <FavoritePage /> },
   { path: "*", element: <NotFoundPage /> },
 ];
 
